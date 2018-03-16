29a91355cac917d3afdf92ee7c03f94b2af2ad48
jsin68 <jsin68@student.monash.edu>
2018-03-17 10:49:16 +1100

    fixed output syntax

--- automatedAlarm.py
+++ automatedAlarm.py
@@ -5,11 +5,11 @@ def automatedAlarm(day, noSchool):
 	weekends = ["saturday","sunday"]
 	weekdays = ["monday","tuesday","wednesday","thursday","friday"]
 	if day.lower() in weekends:
-		return "9:00 am"
+		return "9:00"
 	elif day.lower() in weekdays and noSchool:
-		return "9.30 am " if day.lower() == "monday" else "8.30 am"
+		return "9.30" if day.lower() == "monday" else "8.30"
 	else:
-		return "7.30 am " if day.lower() == "wednesday" else "7.00 am"
+		return "7.30" if day.lower() == "wednesday" else "7.00"
 
 
 
